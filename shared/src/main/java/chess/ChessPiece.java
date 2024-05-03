7714afd40fd4a717d9a75abd333ace0c2214a53f
KProgammer <detectivegreen@gmail.com>
2024-05-03 15:50:52 -0600

    Update Add array for Chessboard, Add color and type to pieces, add pieceMoves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -10,7 +12,13 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private ChessPiece piece_of_interest;               //Placeholder used to figure out what moves a piece can make.
+    private final ChessGame.TeamColor pieceColor;           //A value that holds the team
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -29,14 +37,16 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
+        //throw new RuntimeException("Not implemented");
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
+        //throw new RuntimeException("Not implemented");
     }
 
     /**
@@ -47,6 +57,40 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        //Get the piece that we are interested in moving
+        piece_of_interest = board.getPiece(myPosition);
+
+        // Stores the list of moves
+        ArrayList<Integer> List_of_Moves = new ArrayList<Integer>();
+
+        // Stores the possible positions for each piece
+        int pos_row;
+        int pos_col;
+
+        //Identify the type of piece (KING, QUEEN, etc.) and the find the moves it can do.
+        if (piece_of_interest.type == PieceType.KING){
+
+        }
+        else if (piece_of_interest.type == PieceType.QUEEN){
+
+        }
+        else if (piece_of_interest.type == PieceType.BISHOP){
+            //Working on implementing this code
+            //while((pos_row < 9) && (pos_col<9)){
+            //    List_of_Moves.add([pos_row,pos_col]);
+           // }
+        }
+        else if (piece_of_interest.type == PieceType.KNIGHT){
+
+        }
+        else if (piece_of_interest.type == PieceType.ROOK){
+
+        }
+        else if (piece_of_interest.type == PieceType.PAWN){
+
+        }
+
+        return new ArrayList<>();
+        //throw new RuntimeException("Not implemented");
     }
 }
