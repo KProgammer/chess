e315ceaf3e91a2a356ee39d4abbea1b493aeb07e
KProgammer <detectivegreen@gmail.com>
2024-05-14 10:22:28 -0600

    Deleted extra code and replaced all points where we determined if the new position was on the board using a function from ChessPiece.

--- shared/src/main/java/chess/MoveRook.java
+++ shared/src/main/java/chess/MoveRook.java
@@ -36,7 +36,7 @@ public class MoveRook extends ChessPiece {
             pos_col += 1;
 
             // If the proposed space is not on the board, don't add it.
-            if (pos_col < 1 || pos_col > 8 || pos_row < 1 || pos_col > 8){
+            if (!IsOnBoard(new ChessPosition(pos_row, pos_col))){
                 break;
             }
 
@@ -58,7 +58,7 @@ public class MoveRook extends ChessPiece {
             pos_row -= 1;
 
             // If the proposed space is not on the board, don't add it.
-            if (pos_col < 1 || pos_col > 8 || pos_row < 1 || pos_col > 8){
+            if (!IsOnBoard(new ChessPosition(pos_row, pos_col))){
                 break;
             }
 
@@ -80,7 +80,7 @@ public class MoveRook extends ChessPiece {
             pos_col -= 1;
 
             // If the proposed space is not on the board, don't add it.
-            if (pos_col < 1 || pos_col > 8 || pos_row < 1 || pos_col > 8){
+            if (!IsOnBoard(new ChessPosition(pos_row, pos_col))){
                 break;
             }
 
@@ -102,7 +102,7 @@ public class MoveRook extends ChessPiece {
             pos_row += 1;
 
             // If the proposed space is not on the board, don't add it.
-            if (pos_col < 1 || pos_col > 8 || pos_row < 1 || pos_col > 8){
+            if (!IsOnBoard(new ChessPosition(pos_row, pos_col))){
                 break;
             }
 
@@ -117,79 +117,4 @@ public class MoveRook extends ChessPiece {
 
         return Piece_Moves;
     }
-
-    /**
-     * Adds a move to the list of valid moves
-     *
-     * @param move The position the bishop is to be placed
-     * @return If there should be no more moves in the direction of the given move, this will return false
-     */
-    /*private boolean AddMove(ChessPosition move){
-        //Create a boolean to determine if the space being considered is empty
-        boolean isEmpty = false;
-
-        //Set pos_row and pos_col to starting position
-        //pos_row = position.getRow();
-        //pos_col = position.getColumn();
-
-        //These are if the king is in check.
-        ChessGame possibleGame = new ChessGame();
-        ChessBoard possibleBoard = cur_game.getBoard().clone();
-        ChessMove possibleMove;
-        possibleGame.setBoard(possibleBoard);
-
-        //If the space is empty, add it as a possible move.
-        if(board.getPiece(move) == null){
-            //If the king is in check see if this move will stop the board from being in check
-            if(cur_game.isInCheck(board.getPiece(position).getTeamColor()));{
-                //Add the move possible move
-                possibleMove = new ChessMove(position,move,null);
-                //Clear both squares
-                possibleBoard.addPiece(position,null);
-                possibleBoard.addPiece(possibleMove.getEndPosition(),null);
-                //Set the piece in the new position
-                possibleBoard.addPiece(possibleMove.getEndPosition(), board.getPiece(move));
-
-                //See if the king is still in check.
-                if (possibleGame.isInCheckmate(board.getPiece(move).getTeamColor())){
-                    isEmpty = true;
-                    return isEmpty;
-                }
-                //If not continue
-            }
-            //Add the position to the new list of possible moves.
-            Piece_Moves.add(new ChessMove(position,move,null));
-            isEmpty = true;
-            return isEmpty;
-        }
-        //If the space contains an opposing piece, add the space as a possible move and then end all
-        //possible moves in this direction.
-        else if (board.getPiece(move).getTeamColor() != board.getPiece(position).getTeamColor()) {
-            //If the king is in check see if this move will stop the board from being in check
-            if(cur_game.isInCheck(board.getPiece(position).getTeamColor()));{
-                //Add the move possible move
-                possibleMove = new ChessMove(position,move,null);
-                //Clear both squares
-                possibleBoard.addPiece(position,null);
-                possibleBoard.addPiece(possibleMove.getEndPosition(),null);
-                //Set the piece in the new position
-                possibleBoard.addPiece(possibleMove.getEndPosition(), board.getPiece(move));
-
-                //See if the king is still in check.
-                if (possibleGame.isInCheckmate(board.getPiece(move).getTeamColor())){
-                    isEmpty = true;
-                    return isEmpty;
-                }
-                //If not continue
-            }
-            //Add the position to the new list of possible moves.
-            Piece_Moves.add(new ChessMove(position,move,null));
-            return isEmpty;
-        }
-        //If the space contains a piece on the same team, end all possible moves in this direction.
-        else {
-            return isEmpty;
-        }
-
-    }*/
 }
