328d24b71a208594e696379d3f471ee1a65414e9
KProgammer <detectivegreen@gmail.com>
2024-05-24 13:49:15 -0600

    Coded all of AuthorizationDAO

--- server/src/main/java/dataaccess/AuthorizationDAO.java
+++ server/src/main/java/dataaccess/AuthorizationDAO.java
@@ -1,21 +1,104 @@
 package dataaccess;
 
 import model.Authorization;
+import java.util.*;
 
 public class AuthorizationDAO {
-    public Record createAuth(String authToken, String username){
-        return new Authorization(authToken, username);
+
+    Map<String, Record> ListAuthtokenUser = new Map<>() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean containsKey(Object key) {
+            return false;
+        }
+
+        @Override
+        public boolean containsValue(Object value) {
+            return false;
+        }
+
+        @Override
+        public Record get(Object key) {
+            return null;
+        }
+
+        @Override
+        public Record put(String key, Record value) {
+            return null;
+        }
+
+        @Override
+        public Record remove(Object key) {
+            return null;
+        }
+
+        @Override
+        public void putAll(Map<? extends String, ? extends Record> m) {
+
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public Set<String> keySet() {
+            return Set.of();
+        }
+
+        @Override
+        public Collection<Record> values() {
+            return List.of();
+        }
+
+        @Override
+        public Set<Entry<String, Record>> entrySet() {
+            return Set.of();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+    };
+
+    public void createAuth(String username){
+        String authToken = UUID.randomUUID().toString();
+
+        //Make sure that the authToken is unique.
+        while(ListAuthtokenUser.get(authToken) != null) {
+            authToken = UUID.randomUUID().toString();
+        }
+
+        ListAuthtokenUser.put(authToken, new Authorization(authToken, username));
     }
 
-    /*public Record getAuth(String authToken){
+    public Record getAuth(String authToken){
+        return ListAuthtokenUser.get(authToken);
+    }
 
-    }*/
+    public void deleteAuth(String authToken){
+        ListAuthtokenUser.remove(authToken);
+    }
 
-    /*public void deleteAuth(String authToken){
-        //
-    }*/
-
-    /*public void clear(){
-
-    **/
+    public void clear(){
+        ListAuthtokenUser.clear();
+    }
 }
+
+
