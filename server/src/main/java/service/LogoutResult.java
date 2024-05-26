5c2075576a0aa6f9ac77776b6b41cdca3db2fcdc
KProgammer <detectivegreen@gmail.com>
2024-05-25 19:10:25 -0600

    Updated LogoutService and corresonding requests.

--- server/src/main/java/service/LogoutResult.java
+++ server/src/main/java/service/LogoutResult.java
@@ -1,4 +1,10 @@
 package service;
 
 public class LogoutResult {
+    private final String message;
+
+    public LogoutResult (String message){
+
+        this.message = message;
+    }
 }
--- server/src/main/java/service/LogoutService.java
+++ server/src/main/java/service/LogoutService.java
@@ -1,4 +1,17 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static server.Server.authorizationObject;
+
 public class LogoutService {
+    //logout
+    public LogoutResult logout(LogoutRequest request){
+        if(authorizationObject.getAuth(request.getAuthToken()) == null){
+            return new LogoutResult("Error: unauthorized");
+        }
+
+        return new LogoutResult(null);
+    }
 }
