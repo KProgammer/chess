27d202a7e52f8b073f7a4dcb951fa4c30443126b
KProgammer <detectivegreen@gmail.com>
2024-05-25 19:03:03 -0600

    Updated LoginService and corresponding requests.

--- server/src/main/java/service/LoginResult.java
+++ server/src/main/java/service/LoginResult.java
@@ -1,4 +1,14 @@
 package service;
 
 public class LoginResult {
+    private final String username;
+    private final String password;
+    private final String message;
+
+    public LoginResult (String username, String password, String message){
+
+        this.username = username;
+        this.password = password;
+        this.message = message;
+    }
 }
--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,4 +1,18 @@
 package service;
 
+import java.util.Objects;
+
+import static server.Server.authorizationObject;
+import static server.Server.userObject;
+
 public class LoginService {
+    //login
+    public RegisterResult login(RegisterRequest request){
+        if(userObject.getUser(request.getUsername()) == null ||
+                !Objects.equals(userObject.getUser(request.getUsername()).password(), request.getPassword())){
+            return new RegisterResult(null,null,"Error: unauthorized");
+        }
+
+        return new RegisterResult(request.getUsername(),authorizationObject.createAuth(request.getUsername()),null);
+    }
 }
