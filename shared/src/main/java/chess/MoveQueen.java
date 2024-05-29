c10e8734bc5cfe0e04d6317cf49bce6d42c23fc0
KProgammer <detectivegreen@gmail.com>
2024-05-28 22:43:52 -0600

    Made MoveQueens shorter and eliminated commented out code.

--- shared/src/main/java/chess/MoveQueen.java
+++ shared/src/main/java/chess/MoveQueen.java
@@ -34,7 +34,41 @@ public class MoveQueen extends ChessPiece {
 
     public ArrayList<ChessMove> pieceMoves(ChessPosition myPosition) {
 
-        //Calculate moves for the upper right diagonal or towards row 8, col 8
+        for (int i = -1; i < 2; i++){
+            for(int j = -1; j < 2; j++){
+                while((posRow < 8 && posCol < 8 && Integer.signum(i) > 0 && Integer.signum(j) > 0) ||
+                        (posCol < 8 && Integer.signum(i) == 0 && Integer.signum(j) > 0) ||
+                        (posRow > 1 && posCol < 8 && Integer.signum(i) < 0 && Integer.signum(j) > 0) ||
+                        (posRow > 1 && Integer.signum(i) < 0 && Integer.signum(j) == 0) ||
+                        (posRow > 1 && posCol > 1 && Integer.signum(i) < 0 && Integer.signum(j) < 0) ||
+                        (posCol > 1 && Integer.signum(i) == 0 && Integer.signum(j) < 0) ||
+                        (posRow < 8 && posCol > 1 && Integer.signum(i) > 0 && Integer.signum(j) < 0) ||
+                        (posRow < 8 && Integer.signum(i) > 0 && Integer.signum(j) == 0)){
+                    //Add the values
+                    posRow += i;
+                    posCol += j;
+
+                    // If the proposed space is not on the board, don't add it.
+                    if (!isOnBoard(new ChessPosition(posRow, posCol))){
+                        break;
+                    }
+
+                    //Attempt to add the move
+                    addPiece(pieceMoves, posRow, posCol);
+
+                    //if there is any piece you must stop advancing
+                    if(board.getPiece(new ChessPosition(posRow, posCol)) != null) {
+                        break;
+                    }
+                }
+                //Reset the values of pos_row and pos_col
+                posRow = myPosition.getRow();
+                posCol = myPosition.getColumn();
+            }
+        }
+
+
+        /*//Calculate moves for the upper right diagonal or towards row 8, col 8
         while(posRow < 8 && posCol < 8){
             //Add the values
             posRow += 1;
@@ -209,7 +243,7 @@ public class MoveQueen extends ChessPiece {
             if(board.getPiece(new ChessPosition(posRow, posCol)) != null) {
                 break;
             }
-        }
+        }*/
 
         return pieceMoves;
     }
