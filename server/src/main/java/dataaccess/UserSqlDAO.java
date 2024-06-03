28bc9972f3ea79808fbefc0da6c244c655c70629
KProgammer <detectivegreen@gmail.com>
2024-06-03 16:24:33 -0600

    Update UserSqlDAO to actually return information obtained from SQL.

--- server/src/main/java/dataaccess/UserSqlDAO.java
+++ server/src/main/java/dataaccess/UserSqlDAO.java
@@ -16,45 +16,52 @@ public class UserSqlDAO implements UserDAO{
     @Override
     public void createUser(String username, String password, String email) throws Exception{
         try (var conn = DatabaseManager.getConnection()) {
-            try (var preparedStatement = conn.prepareStatement("INSERT INTO user (username, password, email) " +
-                    "VALUES ('"+username+"', '"+password+", '"+email+"'")) {
-                var rs = preparedStatement.executeQuery();
-                rs.next();
+            try (var preparedStatement = conn.prepareStatement("INSERT INTO user (username, password, email) VALUES (?, ?, ?)")) {
+                preparedStatement.setString(1,username);
+                preparedStatement.setString(2,password);
+                preparedStatement.setString(3,email);
+                preparedStatement.executeUpdate();
             }
         }
     }
 
     @Override
     public User getUser(String username) throws Exception{
+        String password = null;
+        String email = null;
+
         try (var conn = DatabaseManager.getConnection()) {
-            try (var preparedStatement = conn.prepareStatement("SELECT user, password, email FROM user WHERE" +
-                    "username = '"+username+"' LIMIT 1")) {
-                var rs = preparedStatement.executeQuery();
-                rs.next();
+            try (var preparedStatement = conn.prepareStatement("SELECT user, password, email FROM user WHERE username = ? LIMIT 1")) {
+                preparedStatement.setString(1,username);
+                try (var rs = preparedStatement.executeQuery()) {
+                    while (rs.next()){
+                        password = rs.getString("password");
+                        email = rs.getString("email");
+                    }
+                }
             }
         }
-        return null;
+        return new User(username,password,email);
     }
 
     @Override
     public void clear() throws Exception{
         try (var conn = DatabaseManager.getConnection()) {
             try (var preparedStatement = conn.prepareStatement("DELETE FROM user")) {
-                var rs = preparedStatement.executeQuery();
-                rs.next();
+                preparedStatement.executeUpdate();
             }
         }
     }
 
     private final String[] createStatements = {
-            //`id` int NOT NULL AUTO_INCREMENT,
+            //`json` TEXT DEFAULT NULL,
             """
             CREATE TABLE IF NOT EXISTS  user (
+              `id` int NOT NULL AUTO_INCREMENT
               `username` varchar(256),
               `password` varchar(256),
               `email` varchar(256),
-              `json` TEXT DEFAULT NULL,
-              PRIMARY KEY (`username`),
+              PRIMARY KEY (`id`),
               INDEX(username),
               INDEX(password),
               INDEX(email)
