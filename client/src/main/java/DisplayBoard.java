9a4317ebd57462af2e29ebfdc9ec9df8b293e8e6
KProgammer <detectivegreen@gmail.com>
2024-06-06 21:01:09 -0600

    Almost have a white chessboard generator. Just need to do the sidebars with the numbered rows.

--- client/src/main/java/DisplayBoard.java
+++ client/src/main/java/DisplayBoard.java
@@ -1,5 +1,7 @@
+import chess.ChessBoard;
 import chess.ChessGame;
 import chess.ChessPiece;
+import chess.ChessPosition;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
@@ -9,22 +11,24 @@ import static ui.EscapeSequences.*;
 
 public class DisplayBoard {
     private static final int BOARD_SIZE_IN_SQUARES = 8;
-    private static final int SQUARE_SIZE_IN_CHARS = 8;
-    private static final int LINE_WIDTH_IN_CHARS = 1;
+    private static final int SQUARE_SIZE_IN_CHARS = 3;
+    private static final int LINE_WIDTH_IN_CHARS = 0;
+    private static int pos_row = 1;
+    private static int pos_col = 1;
     private static final String EMPTY = "   ";
     private static final String X = " X ";
     private static final String O = " O ";
     private static Random rand = new Random();
 
 
-    public static void main(String[] args, ChessGame.TeamColor teamColor) {
+    public static void main(String[] args, ChessGame.TeamColor teamColor, ChessGame chessGame) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
         out.print(ERASE_SCREEN);
 
         drawHeaders(out, teamColor);
 
-        drawTicTacToeBoard(out);
+        drawTicTacToeBoard(out, chessGame, teamColor);
 
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_WHITE);
@@ -34,11 +38,12 @@ public class DisplayBoard {
 
         setBlack(out);
 
-        String[] headers = {"a", "b", "c", "d", "e", "f", "g", "h"};
+        String[] headers = {" a ", " b ", " c ", " d ", " e ", " f ", " g ", " h "};
+        //String[] headers = {"TIC", "TAC", "TOE", "TIC", "TAC", "TOE", "TIC", "TAC"};
 
         if(teamColor == ChessGame.TeamColor.BLACK ) {
             //String[] headers = {"TIC", "TAC", "TOE"};
-            headers = new String[]{"h", "g", "f", "e", "d", "c", "b", "a"};
+            headers = new String[]{" h ", " g ", " f ", " e ", " d ", " c ", " b ", " a "};
         }
 
         for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
@@ -70,11 +75,12 @@ public class DisplayBoard {
         setBlack(out);
     }
 
-    private static void drawTicTacToeBoard(PrintStream out) {
+    private static void drawTicTacToeBoard(PrintStream out, ChessGame chessGame, ChessGame.TeamColor teamColor) {
 
         for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
 
-            drawRowOfSquares(out);
+            drawRowOfSquares(out, chessGame, teamColor);
+            pos_row += 1;
 
             if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
                 drawVerticalLine(out);
@@ -83,18 +89,31 @@ public class DisplayBoard {
         }
     }
 
-    private static void drawRowOfSquares(PrintStream out) {
+    private static void drawRowOfSquares(PrintStream out, ChessGame chessGame, ChessGame.TeamColor teamColor) {
 
         for (int squareRow = 0; squareRow < SQUARE_SIZE_IN_CHARS; ++squareRow) {
             for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
                 setWhite(out);
+
+                pos_col = (boardCol + 1);
+
+                if((boardCol%2 == 0) && (pos_row%2 == 0) ||
+                        (boardCol%2 != 0 && (pos_row%2 != 0))){
+                    setBlack(out);
+                }
 
                 if (squareRow == SQUARE_SIZE_IN_CHARS / 2) {
                     int prefixLength = SQUARE_SIZE_IN_CHARS / 2;
                     int suffixLength = SQUARE_SIZE_IN_CHARS - prefixLength - 1;
 
                     out.print(EMPTY.repeat(prefixLength));
-                    printPlayer(out, rand.nextBoolean() ? X : O);
+                    //printPlayer(out, rand.nextBoolean() ? X : O);
+                    if(chessGame.getBoard().getPiece(new ChessPosition(pos_row,pos_col)) != null) {
+                        printPlayer(out, PieceSymbol(chessGame.getBoard().getPiece(new ChessPosition(pos_row, pos_col))),
+                                chessGame.getBoard().getPiece(new ChessPosition(pos_row, pos_col)).getTeamColor());
+                    } else {
+                        printPlayer(out, "   ", null);
+                    }
                     out.print(EMPTY.repeat(suffixLength));
                 }
                 else {
@@ -128,6 +147,24 @@ public class DisplayBoard {
         }
     }
 
+    private static String PieceSymbol(ChessPiece chessPiece){
+        if (chessPiece == null){
+            return "   ";
+        } else if(chessPiece.getPieceType() == ChessPiece.PieceType.QUEEN){
+            return " Q ";
+        } else if (chessPiece.getPieceType() == ChessPiece.PieceType.KING) {
+            return " K ";
+        } else if (chessPiece.getPieceType() == ChessPiece.PieceType.BISHOP) {
+            return " B ";
+        } else if (chessPiece.getPieceType() == ChessPiece.PieceType.KNIGHT) {
+            return " N ";
+        } else if (chessPiece.getPieceType() == ChessPiece.PieceType.ROOK) {
+            return " R ";
+        } else {
+            return " P ";
+        }
+    }
+
     private static void setWhite(PrintStream out) {
         out.print(SET_BG_COLOR_WHITE);
         out.print(SET_TEXT_COLOR_WHITE);
@@ -143,12 +180,17 @@ public class DisplayBoard {
         out.print(SET_TEXT_COLOR_BLACK);
     }
 
-    private static void printPlayer(PrintStream out, String player) {
-        out.print(SET_BG_COLOR_WHITE);
-        out.print(SET_TEXT_COLOR_BLACK);
+    private static void printPlayer(PrintStream out, String player, ChessGame.TeamColor teamColor) {
+        //out.print(SET_BG_COLOR_WHITE);
+
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            out.print(SET_TEXT_COLOR_RED);
+        } else {
+            out.print(SET_TEXT_COLOR_LIGHT_GREY);
+        }
 
         out.print(player);
 
-        setWhite(out);
+        //setWhite(out);
     }
 }
